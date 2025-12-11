6dfb86c91ba8c815d1963fbf16f51814fb92b2ff
Chandan <90612000+Pogopeck@users.noreply.github.com>
2025-12-11 20:06:13 +0530

    removed API key

--- llname.py
+++ llname.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyBE7W5QfYkIEYnEMCO_NVOHES2hNB3qL3Y")
+genai.configure(api_key="")
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
         print(m.name)
